fa3e8944296b5763272c5ecd365bc70c06786ebb
burakkurtarir <burak.kurtarir35@hotmail.com>
2024-02-10 15:03:20 +0300

    added insertion sort

--- main.dart
+++ main.dart
@@ -1,7 +1,10 @@
 import 'bubble_sort.dart';
+import 'insertion_sort.dart';
 
 void main() {
   final arr = [12, -5, 8, 3, -24, 0, 54, 35];
-  bubbleSort(arr);
+  print(arr);
+  // bubbleSort(arr);
+  insertionSort(arr);
   print(arr);
 }
