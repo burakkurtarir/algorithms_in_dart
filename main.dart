f780c864291a5cbbbb403ab670dcc427d0a6d168
burakkurtarir <burak.kurtarir35@hotmail.com>
2024-02-12 00:49:41 +0300

    updated

--- main.dart
+++ main.dart
@@ -3,6 +3,7 @@ import 'binary_search.dart';
 void main() {
   // final arr = [12, -5, 8, 3, -24, 0, 54, 35, 99];
   // print(arr);
+  // quickSort(arr, 0, arr.length - 1);
 
   final arrSorted = [-24, -5, 0, 3, 8, 12, 35, 54, 99];
   final i = binarySearch(arrSorted, 54);
