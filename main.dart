704a830a2882b89a34d291de87794a5239f4afad
burakkurtarir <burak.kurtarir35@hotmail.com>
2024-02-12 00:47:29 +0300

    added binary search

--- main.dart
+++ main.dart
@@ -1,12 +1,10 @@
-import 'bubble_sort.dart';
-import 'insertion_sort.dart';
-import 'merge_sort.dart';
-import 'quick_sort.dart';
+import 'binary_search.dart';
 
 void main() {
-  final arr = [12, -5, 8, 3, -24, 0, 54, 35, 99];
-  print(arr);
-  // bubbleSort(arr);
-  quickSort(arr, 0, arr.length - 1);
-  print(arr);
+  // final arr = [12, -5, 8, 3, -24, 0, 54, 35, 99];
+  // print(arr);
+
+  final arrSorted = [-24, -5, 0, 3, 8, 12, 35, 54, 99];
+  final i = binarySearch(arrSorted, 54);
+  print(i);
 }
